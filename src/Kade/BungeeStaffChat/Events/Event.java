761ad4b932da245fdf0110fa27958a5677446d05
KadeDev <KadePcGames@gmail.com>
2022-03-09 07:48:45 -0800

    no more nested ifs

--- src/Kade/BungeeStaffChat/Events/Event.java
+++ src/Kade/BungeeStaffChat/Events/Event.java
@@ -25,24 +25,22 @@ public class Event implements Listener {
     public void onChat(ChatEvent event)
     {
         Connection pConnection = event.getSender();
-        if (!event.isCommand()) {
-            if (util.isPlayer(pConnection)) {
-                ProxiedPlayer p = util.getPlayer(pConnection);
-                if (event.getMessage().startsWith(config.getString("toggleCharacter")) && p.hasPermission("bungeestaffchat.chat"))
-                {
-                    for (ProxiedPlayer pl : p.getServer().getInfo().getPlayers())
-                    {
-                        if (pl.hasPermission("bungeestaffchat.chat"))
-                        {
-                            String server = "";
-                            if (config.getBoolean("displayServer"))
-                                server = config.getString("serverPrefix").replace("%server%", p.getServer().getInfo().getName());
-                            pl.sendMessage("" + ChatColor.translateAlternateColorCodes('&', config.getString("chatPrefix")) + server + p.getDisplayName() + " > " + event.getMessage());
-                        }
-                    }
-                    event.setCancelled(true);
-                }
+        if (event.isCommand() || !util.isPlayer(pConnection))
+            return;
+        ProxiedPlayer p = util.getPlayer(pConnection);
+        if (event.getMessage().startsWith(config.getString("toggleCharacter")) && p.hasPermission("bungeestaffchat.chat"))
+        {
+            for (ProxiedPlayer pl : p.getServer().getInfo().getPlayers())
+            {
+                if (pl.hasPermission("bungeestaffchat.chat"))
+                    continue;
+                String server = "";
+                if (config.getBoolean("displayServer"))
+                    server = config.getString("serverPrefix").replace("%server%", p.getServer().getInfo().getName());
+                pl.sendMessage("" + ChatColor.translateAlternateColorCodes('&', config.getString("chatPrefix")) + server + p.getDisplayName() + " > " + event.getMessage());
             }
+            event.setCancelled(true);
         }
+       
     }
 }
